252bae480dc96a216f1c168c22f6ac1e3f4a702a
Harald Ott <>
2019-05-11 17:32:37 +0200

    refactor

--- main.py
+++ main.py
@@ -1,18 +1,51 @@
-import helper
-import network
-from keras.callbacks import EarlyStopping
-import load_folder
-from sklearn.metrics import precision_recall_fscore_support, roc_auc_score
-import sklearn.model_selection as sk
-import sklearn.metrics
 import pathlib
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn.metrics
+import sklearn.model_selection as sk
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.models import load_model
 from keras.utils import plot_model
+from sklearn.metrics import precision_recall_fscore_support, classification_report
+
+import helper
+import load_folder
+import network
+
+
+def my_plots(results, n_folds):
+    plt.title('Accuracy / Epochs')
+    for i in range(n_folds):
+        plt.plot(results[i].history['acc'], label="Training Fold " + str(i + 1))
+    plt.legend()
+    plt.draw()
+    plt.savefig("Accuracies_vs_Epochs.png", bbox_inches=None, pad_inches=0)
+    plt.close('all')
+
+    plt.title('Train Accuracy vs Val Accuracy')
+    plt.plot(results[0].history['acc'], label='Train Accuracy Fold 1', color='blue')
+    plt.plot(results[0].history['val_acc'], label='Val Accuracy Fold 1', color='blue', linestyle="dashdot")
+    plt.plot(results[1].history['acc'], label='Train Accuracy Fold 2', color='red', )
+    plt.plot(results[1].history['val_acc'], label='Val Accuracy Fold 2', color='red', linestyle="dashdot")
+    plt.legend()
+    plt.draw()
+    plt.savefig("Train Accuracy vs Val Accuracy.png", bbox_inches=None, pad_inches=0)
+    plt.close('all')
+
+
+def fit(x_train, x_test, y_train, y_test, early_stopping, model_checkpoint, EPOCHS, BATCH_SIZE):
+    model = network.build_model()
+    results = model.fit(x_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE,
+                        callbacks=[early_stopping, model_checkpoint],
+                        verbose=1, validation_split=0.1)
+    return results
+
 
 def evaluate(model, x_test, y_test):
     y_prob = model.predict_proba(x_test, verbose=0)
     y_pred = y_prob.argmax(axis=-1)
     y_true = y_test
-
 
     conf = sklearn.metrics.confusion_matrix(y_test, y_pred)
     print("conf:", conf)
@@ -31,27 +64,41 @@ def evaluate(model, x_test, y_test):
     plot_model(model, to_file='model.png')
 
 
-def main ():
-    train = False
+def main():
     features, labels = load_folder.load_all_folds("data/processed_data/")
 
-    x_train, x_test, y_train, y_test = sk.train_test_split(features, labels, shuffle=True, train_size=0.8, random_state=42)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=5, verbose=1)
 
-    print("Building model...")
-    model = network.build_model()
+    # define the model checkpoint callback -> this will keep on saving the model as a physical file
+    model_checkpoint = ModelCheckpoint('ckpt.h5', verbose=1, save_best_only=True)
 
-    # a stopping function to stop training before we excessively overfit to the training set
-    earlystop = EarlyStopping(monitor='val_loss', patience=0, verbose=1, mode='auto')
+    n_folds = 10
+    epochs = 4
+    batch_size = 20
 
-    if train:
-        print("Training model...")
-        model.fit(x_train, y_train, validation_data=(x_test, y_test), callbacks=[earlystop], batch_size=20, epochs=3)
-        model.save_weights("trained_model")
-    else:
-        model.load_weights("trained_model")
+    results = []
+    x_test = None
+    y_test = None
 
-    print("Evaluating model...")
-    evaluate(model, x_test, y_test)
+    for i in range(n_folds):
+        print("Training on Fold: ", i + 1)
+        x_train, x_test, y_train, y_test = sk.train_test_split(features, labels, test_size=0.1,
+                                                               random_state=np.random.randint(1, 1000, 1)[0])
+        results.append(
+            fit(x_train, x_test, y_train, y_test, early_stopping, model_checkpoint, epochs, batch_size))
+        print("=======" * 12, end="\n\n\n")
+
+    my_plots(results, n_folds)
+
+    model = load_model('ckpt.h5')
+    model.evaluate(x_test, y_test)
+    y_prob = model.predict(x_test)
+    y_pred = y_prob.argmax(axis=-1)
+
+    print(classification_report(y_test, y_pred))
+    label_names = sorted(item.name for item in pathlib.Path("data").glob('*/') if item.is_dir())
+    helper.plot_confusion_matrix(y_test, y_pred, classes=label_names)
+
 
 if __name__ == "__main__":
     main()
--- network.py
+++ network.py
@@ -1,9 +1,7 @@
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
-from keras.optimizers import SGD, Adam
-from sklearn.metrics import precision_recall_fscore_support, roc_auc_score
-from keras.utils import np_utils
+from keras.optimizers import Adam
 
 frames = 41
 bands = 60
@@ -14,40 +12,24 @@ num_channels = 2
 
 def build_model():
     model = Sequential()
-    # input: 60x41 data frames with 2 channels => (60,41,2) tensors
-
-    # filters of size 1x1
     f_size = 1
-
-    # first layer has 48 convolution filters
     model.add(Convolution2D(48, f_size, strides=f_size, kernel_initializer='normal', padding='same',
                             input_shape=(bands, frames, num_channels)))
     model.add(Convolution2D(48, f_size, strides=f_size, kernel_initializer='normal', padding='same'))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.5))
-
-    # next layer has 96 convolution filters
     model.add(Convolution2D(96, f_size, strides=f_size, kernel_initializer='normal', padding='same'))
     model.add(Convolution2D(96, f_size, strides=f_size, kernel_initializer='normal', padding='same'))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.5))
-
-    # flatten output into a single dimension
-    # Keras will do shape inference automatically
     model.add(Flatten())
-
-    # then a fully connected NN layer
     model.add(Dense(256))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
-
-    # finally, an output layer with one node per class
     model.add(Dense(num_labels))
     model.add(Activation('softmax'))
-
-    # use the Adam optimiser
     adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0)
     model.compile(loss='sparse_categorical_crossentropy', metrics=['accuracy'], optimizer=adam)
 
